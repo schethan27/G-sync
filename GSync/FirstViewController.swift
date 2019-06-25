d61e417ad7c695678bab1e652122de9bad860559
schethan27 <schethan27@gmail.com>
2019-06-25 07:46:31 +0530

    Initial code check-in

---
+++ GSync/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  GSync
+//
+//  Created by Chethan on 24/06/19.
+//  Copyright © 2019 CS. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
